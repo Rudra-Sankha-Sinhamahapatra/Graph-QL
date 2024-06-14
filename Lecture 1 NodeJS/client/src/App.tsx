4b6e52b07a7c62764a417a034620055baba2eb0a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-14 22:30:35 +0530

    Made some enhancements for typescript

--- Lecture 1 NodeJS/client/src/App.tsx
+++ Lecture 1 NodeJS/client/src/App.tsx
@@ -17,15 +17,43 @@ const query = gql`
   }
 `;
 
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+  };
+}
+
 function App() {
-  const { data, loading } = useQuery(query);
+  const { data, loading } = useQuery<{ getTodos: Todo[] }>(query);
 
   if (loading) {
     return <div>Loading....</div>;
   }
+  
   return (
-    <div>
-      <div className="App">{JSON.stringify(data)}</div>
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Todo</th>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.getTodos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo?.user ? todo?.user?.name : "null"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
